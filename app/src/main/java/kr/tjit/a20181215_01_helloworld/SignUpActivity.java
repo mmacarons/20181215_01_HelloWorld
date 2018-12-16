94e8e951372beb26be70da00ac5413b4aab993a5
mmacarons <manymany.macarons@gmail.com>
2018-12-16 10:54:58 +0900

    findviewbyid

--- app/src/main/java/kr/tjit/a20181215_01_helloworld/SignUpActivity.java
+++ app/src/main/java/kr/tjit/a20181215_01_helloworld/SignUpActivity.java
@@ -2,12 +2,21 @@ package kr.tjit.a20181215_01_helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpBtn = findViewById(R.id.signUpBtn);
+        signUpBtn.setText("회원가입");
+    }
+
     }
 }
